bb86f88ef75cdfb47d39450f67dee25314db3f5a
Supanat <supanat.ji@gmail.com>
2019-02-05 09:41:29 +0700

    update chapter3

--- 03_ResourceSystem/HelloViews1/app/src/main/java/com/example/helloviews1/MainActivity.java
+++ 03_ResourceSystem/HelloViews1/app/src/main/java/com/example/helloviews1/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_quiz);
+        setContentView(R.layout.activity_main);
     }
 }
